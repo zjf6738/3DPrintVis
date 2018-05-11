2982573f528ecf2b0328e14ab3ab37fce4e613bb
zjf6738 <zjf6738@163.com>
2018-05-11 09:41:22 +0800

    1、增加了IntPtr转IPLImage转Mat的代码

--- 3DPrintVis/FirstStepMulti/Program.cs
+++ 3DPrintVis/FirstStepMulti/Program.cs
@@ -14,7 +14,7 @@ namespace FirstStepMulti
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
